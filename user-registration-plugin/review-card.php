e86597d5cf504823d0cd077b97942e17749f9f3d
Utsav Tamrakar <utsavtmr@gmail.com>
2023-06-23 19:55:25 +0545

    Update review-card.php
    Added review stars to indicate the number of review user has left. Also, Added form above the rendered display cards to filter according to latest user creation and all dates. Dynamic yet to be made.

--- user-registration-plugin/review-card.php
+++ user-registration-plugin/review-card.php
@@ -27,13 +27,58 @@
                         </style>
 
                         <div class="reviews-grid">
+                            <!-- form to apply filters to the rendered data -->
+                        <form action="<?php echo esc_url(home_url('/')); ?>" method="GET" class="review-filters">
+                                    <label for="rating-filter">Filter by Rating:</label>
+                                    <select name="rating-filter" id="rating-filter">
+                                        <option value="">All Ratings</option>
+                                        <option value="1">1 Star</option>
+                                        <option value="2">2 Stars</option>
+                                        <option value="3">3 Stars</option>
+                                        <option value="4">4 Stars</option>
+                                        <option value="5">5 Stars</option>
+                                    </select>
+
+                                    <label for="date-filter">Filter by Registration Date:</label>
+                                    <select name="date-filter" id="date-filter">
+                                        <option value="">All Dates</option>
+                                        <option value="latest">Latest Registered</option>
+                                    </select>
+
+                                    <input type="submit" value="Apply Filter">
+                                </form>
                             <?php foreach ($user_records as $record) { ?>
                                 <div class="review-card">
                                 <div class="full-name">
                                         Full Name: <?php echo !empty($record->full_name) ? $record->full_name : 'N/A'; ?>
                                     </div>
                                     <div class="review-rating">
-                                        Rating: <?php echo !empty($record->review_rating) ? $record->review_rating : 'N/A'; ?>
+                                        Rating: <?php if(!empty($record->review_rating)){ 
+                                        if($record->review_rating == 1){
+                                            echo '<span class="fa fa-star checked"></span>';
+                                        }elseif($record->review_rating == 2){
+                                            echo '<span class="fa fa-star checked"></span>';
+                                            echo '<span class="fa fa-star checked"></span>';
+                                        }elseif($record->review_rating == 3){
+                                            echo '<span class="fa fa-star checked"></span>';
+                                            echo '<span class="fa fa-star checked"></span>';
+                                            echo '<span class="fa fa-star checked"></span>';
+                                        }elseif($record->review_rating == 4){
+                                            echo '<span class="fa fa-star checked"></span>';
+                                            echo '<span class="fa fa-star checked"></span>';
+                                            echo '<span class="fa fa-star checked"></span>';
+                                            echo '<span class="fa fa-star checked"></span>';
+                                        }elseif($record->review_rating == 5){
+                                            echo '<span class="fa fa-star checked"></span>';
+                                            echo '<span class="fa fa-star checked"></span>';
+                                            echo '<span class="fa fa-star checked"></span>';
+                                            echo '<span class="fa fa-star checked"></span>';
+                                            echo '<span class="fa fa-star checked"></span>';
+                                        }
+                                        }else{
+                                            echo 'N/A'; 
+                                        }
+                                        ?>
                                     </div>
                                     <div class="review-description">
                                         <?php echo !empty($record->review_description) ? $record->review_description : 'N/A'; ?>
